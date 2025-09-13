880805bc00b0db8f973b861b821d1c3d50ce8a3c
itsluckysharma01 <panditluckysharma42646@gmail.com>
2025-09-13 22:08:06 +0530

    list crated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div>
+      <Navbar />
       <h1>Shopping Cart</h1>
-      
     </div>
   );
 }
